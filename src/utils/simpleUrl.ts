e8f5f62db6e68cc2d30125adf35a5644b0227cdb
annalhq <155549764+annalhq@users.noreply.github.com>
2025-02-28 22:14:31 +0530

    fix: url bug

--- src/utils/simpleUrl.ts
+++ src/utils/simpleUrl.ts
@@ -2,7 +2,12 @@ import memoizeOne from 'memoize-one';
 
 export const simpleUrl = memoizeOne(
   (url: string, withPath?: boolean): string => {
-    const newUrl = new URL(url);
+    let newUrl;
+    try {
+      newUrl = new URL(url);
+    } catch (e) {
+      return '';
+    }
     const hostname = newUrl.hostname?.replace('www.', '');
     return withPath ? `${hostname}${newUrl.pathname}` : hostname;
   },
